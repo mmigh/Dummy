bd62d378b9e0bcc6fc451a2806bda67975571a83
Primal_han <hieu30671@gmail.com>
2025-07-14 20:05:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from flask import Flask
+import logging
+import os
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.WARNING)
 
 app = Flask(__name__)
 
@@ -6,5 +11,10 @@ app = Flask(__name__)
 def home():
     return "<h1>Trang web đơn giản dùng Flask để giữ host</h1>"
 
+@app.route("/favicon.ico")
+def favicon():
+    return '', 204
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
